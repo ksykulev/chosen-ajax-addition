63860e92215c7fc7025b3695ac7954bcbdc6cb74
konst <konst@sykulev.com>
2012-02-20 14:59:18 -0500

    adding an example.

---
+++ example/search.php
@@ -0,0 +1,66 @@
+<?php
+$q = trim($_POST['data']['q']);
+
+$states = array(
+	'Alabama',
+	'Alaska',
+	'Arizona',
+	'Arkansas',
+	'California',
+	'Colorado',
+	'Connecticut',
+	'Delaware',
+	'District of Columbia',
+	'Florida',
+	'Georgia',
+	'Hawaii',
+	'Idaho',
+	'Illinois',
+	'Indiana',
+	'Iowa',
+	'Kansas',
+	'Kentucky',
+	'Louisiana',
+	'Maine',
+	'Maryland',
+	'Massachusetts',
+	'Michigan',
+	'Minnesota',
+	'Mississippi',
+	'Missouri',
+	'Montana',
+	'Nebraska',
+	'Nevada',
+	'New Hampshire',
+	'New Jersey',
+	'New Mexico',
+	'New York',
+	'North Carolina',
+	'North Dakota',
+	'Ohio',
+	'Oklahoma',
+	'Oregon',
+	'Pennsylvania',
+	'Puerto Rico',
+	'Rhode Island',
+	'South Carolina',
+	'South Dakota',
+	'Tennessee',
+	'Texas',
+	'Utah',
+	'Vermont',
+	'Virginia',
+	'Virgin Islands',
+	'Washington',
+	'West Virginia',
+	'Wisconsin',
+	'Wyoming'
+);
+
+$results = array();
+foreach($states as $i => $state){
+	if( stristr($state,$q) !== FALSE ){
+		$results[] = array('id' => $i, 'text' => $state);
+	}
+}
+echo json_encode(array('q' => $q, 'results' => $results));
